eb3e53216d0e881055b9a7fc6c55e90a7175c956
Chase Gruber <chasegruber@gmail.com>
2015-10-26 09:07:19 -0400

    build a deck of cards

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var deck = document.getElementById('deck');
+var cards = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+var suits = ['s','h','c','d'];
+for (var i=0;i<cards.length;i++){
+  for (var s=0;s<suits.length;s++){
+    var card = document.createElement('div');
+    card.className = 'card';
+    card.setAttribute('value', cards[i]);
+    card.setAttribute('suit', suits[s]);
+    deck.appendChild(card);
+  }
+}
