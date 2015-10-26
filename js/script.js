6d85c4ac32f88d7ff293c3c629deff12191e460d
Chase Gruber <chasegruber@gmail.com>
2015-10-26 11:16:28 -0400

    functioning tie logic

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ function shuffle(){
 function dealDeck(players){
   //deal deck to X number of players;
   var stackLength = deckList.length/players;
-  //var stacks = [];
   for (var i=0;i<players;i++){
     var stack = {'player':i,'stack':[]};
     stacks.push(stack);
@@ -30,12 +29,12 @@ function dealDeck(players){
       stacks[s].stack.push(deckList.pop());
     }
   }
-  //return stacks;
 }
 
 function getCards(){
   var p1 = stacks[0].stack.splice(0,1)[0];
   var p2 = stacks[1].stack.splice(0,1)[0];
+  console.log(p1.value,p2.value);
   return [p1,p2];
 }
 
@@ -48,33 +47,60 @@ function play(plays){
     stacks[0].stack.push(p1);
     stacks[0].stack.push(p2);
     winner = 0;
-  }else if(p1.value < p2.value){
+  }else if(p2.value > p1.value){
     stacks[1].stack.push(p1);
     stacks[1].stack.push(p2);
     winner = 1;
   }else{
-    draw();
+    burn(plays);
+    return;
   }
-  console.log(stacks[0].stack.length,stacks[1].stack.length);
-  return winner;
+  if (dump.length>0 && winner){
+    while (dump.length>0){
+      stacks[winner].stack.push(dump.pop());
+      console.log(dump.length);
+    }
+  }
+  console.log(stacks[0].stack.length,stacks[1].stack.length, dump.length);
+  //return winner;
 }
 
-function draw(){
+// function draw(){
+//   var burns = getBurnCounts();
+//   var dump1 = stacks[0].stack.splice(0,burns[0]);
+//   var dump2 = stacks[1].stack.splice(0,burns[1]);
+//   dump = (dump.length == 0)?dump1.concat(dump2):dump.concat(dump1).concat(dump2);
+//
+//   console.log(dump);
+//
+//   var winner = play(getCards());
+//   for (var i=0;i<dump.length;i++){
+//     stacks[winner].stack.push(dump[i]);
+//   }
+// }
 
-  var dump1 = stacks[0].stack.splice(0,3);
-  var dump2 = stacks[1].stack.splice(0,3);
-  dump = (dump.length == 0)?dump1.concat(dump2):dump.concat(dump1).concat(dump2);
+function burn(plays){
+  var burns = getBurnCounts();
+  var dump1 = stacks[0].stack.splice(0,burns[0]);
+  var dump2 = stacks[1].stack.splice(0,burns[1]);
 
-  console.log(dump);
-
-  var winner = play(getCards());
-  for (var i=0;i<dump.length;i++){
-    stacks[winner].stack.push(dump[i]);
-  }
+  var theseBurns = plays.concat(dump1,dump2);
+  dump = (dump.length == 0)?theseBurns:dump.concat(theseBurns);
+  console.log(dump,dump.length);
 }
 
-function playGame(){
-  while (stacks[0].stack.length > 0 || stacks[1].stack.length >0){
-    play(getCards());
+function getBurnCounts(){
+  var burns = [];
+  for (var i=0;i<stacks.length;i++){
+    var burn = (stacks[i].stack.length>3)?3:stacks[i].stack.length - 1;
+    burns.push(burn)
   }
+  return burns;
 }
+
+//let's not do this yet...
+// function playGame(){
+//   while (stacks[0].stack.length > 0 || stacks[1].stack.length >0){
+//     play(getCards());
+//   }
+// }
