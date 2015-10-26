19d3703f6d89b4e114e14fdb77b75a0f12b6afac
Chase Gruber <chasegruber@gmail.com>
2015-10-26 09:17:00 -0400

    basic shuffle implementation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var deck = document.getElementById('deck');
 var cards = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 var suits = ['s','h','c','d'];
+var deckList = [];
 for (var i=0;i<cards.length;i++){
   for (var s=0;s<suits.length;s++){
     var card = document.createElement('div');
@@ -8,5 +9,7 @@ for (var i=0;i<cards.length;i++){
     card.setAttribute('value', cards[i]);
     card.setAttribute('suit', suits[s]);
     deck.appendChild(card);
+    deckList.push({'value':cards[i],'suit':suits[s]});
   }
 }
+console.log(deckList);
